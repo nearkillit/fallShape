f4afd2f4636f6ffdc2e7335f4e998aa98240b22f
nearkillit <tatantantatantatatantan@yahoo.co.jp>
2021-12-21 14:29:09 +0900

    heroku2

--- pages/Game.vue
+++ pages/Game.vue
@@ -5,20 +5,15 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import Artwork from "@/components/ArtWork/index.vue";
+import Artwork from '@/components/ArtWork/index.vue'
 
 export default Vue.extend({
-  data () {
-    return {
-      email: "",
-      password: ""
-    }
+  data() {
+    return {}
   },
   components: {
-    Artwork
+    Artwork,
   },
-  methods:{    
-           
-  }  
+  methods: {},
 })
 </script>
